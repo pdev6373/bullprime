c17bdf8a3485df0ac55442be2ddc0481be0d7e5c
pdev6373 <adebayoluborode@gmail.com>
2025-08-14 08:34:06 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const ROUTES = [
   {
