e2233704c0b08a5ac07f4e0b83c31300e2c59498
pdev6373 <adebayoluborode@gmail.com>
2025-08-14 08:02:54 +0100

    update

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -12,14 +12,17 @@ const MapContainer = dynamic(
   () => import('react-leaflet').then((mod) => mod.MapContainer),
   { ssr: false },
 );
+
 const TileLayer = dynamic(
   () => import('react-leaflet').then((mod) => mod.TileLayer),
   { ssr: false },
 );
+
 const Marker = dynamic(
   () => import('react-leaflet').then((mod) => mod.Marker),
   { ssr: false },
 );
+
 const Popup = dynamic(() => import('react-leaflet').then((mod) => mod.Popup), {
   ssr: false,
 });
