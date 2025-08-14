5985fc9291f7e79188fac524ce5e4565d3c89f4e
pdev6373 <adebayoluborode@gmail.com>
2025-08-14 08:20:11 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -98,9 +98,6 @@ export default function Header() {
       <AnimatePresence>
         {showMenu && (
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
             className="fixed inset-0 z-[60] bg-white md:hidden"
           >
